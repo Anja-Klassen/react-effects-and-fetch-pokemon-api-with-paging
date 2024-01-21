9a51263c9220a54f1c670f967bb7f5525e36ceaa
Anja Klaßen <anja.klassen@posteo.de>
2024-01-21 19:04:43 +0100

    add solution

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
 
+const limit = 20; // damit wird die aktuelle Seitenzahl multipliziert
+
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [page, setPage] = useState(0); // wir benötigen state, der uns sagt auf welcher Seite wir sind; initial sind wir auf Seite 0
 
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
+          `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${
+            limit * page
+          }` //limit-Parameter kann hier mitgegeben werden mit template literal
         );
         const data = await response.json();
         setPokemon(data.results);
@@ -17,12 +22,17 @@ export default function PokemonList() {
     }
 
     loadPokemon();
-  }, []);
+  }, [page]);
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button type="button" onClick={() => setPage(page === 0 ? 0 : page - 1)}>
+        Previous Page
+      </button>
+      <button type="button" onClick={() => setPage(page + 1)}>
+        Next Page
+      </button>{" "}
+      {/*Button so verändern, dass der Wert von setPage beim Klicken des Buttons sich um 1 erhöht; dazu nutzen wir den onClick Handler*/}
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
